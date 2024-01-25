3fa0ad8dbe403161d0158e78d1a8cc40542cbf65
Sehan Arandara <it21164330@my.sliit.lk>
2024-01-25 20:21:11 +0530

    New service added

---
+++ Backend/teachers-management/src/test/java/org/schoolmanagement/teachersmanagement/TeachersManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.schoolmanagement.teachersmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachersManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
